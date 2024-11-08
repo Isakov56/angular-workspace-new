17f67a4ef05007213eb67e53eee59611570bebe1
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-11-08 17:08:57 +0100

    cleaned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedLayoutComponent } from 'shared-layout';
-import { DashboardComponent } from '../../projects/shared/components/dashboard/dashboard.component';
 import { routes, routeConfig } from './app.routes';
-import { RouteConfig } from '../../models/models';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   
-  imports: [RouterOutlet, SharedLayoutComponent, DashboardComponent],
+  imports: [RouterOutlet, SharedLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
