f43576625c538b8f62ebfd684aa95a5bdde5ea52
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-11-08 17:23:44 +0100

    routes added

---
+++ projects/routes/route2/src/lib/route2.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-route2',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      route2 works!
+    </p>
+  `,
+  styles: ``
+})
+export class Route2Component {
+
+}
---
+++ projects/routes/route2/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of route2
+ */
+
+export * from './lib/route2.service';
+export * from './lib/route2.component';
